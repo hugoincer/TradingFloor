3ba67430654f738fdb0f516c3066ef1ae67d3aff
hugoincer <hugoincer@gmail.com>
2016-11-16 07:07:16 +0300

    Migrations fixed, added to functions.

--- resources/migrations/20161116032013-add-all-tables.up.sql
+++ resources/migrations/20161116032013-add-all-tables.up.sql
@@ -6,27 +6,24 @@ CREATE TABLE "tradingfloor_dev"."public"."user" (
                 "email" VARCHAR(50) NOT NULL,
                 "password" VARCHAR(300),
                 "oneTimePass" VARCHAR(300),
-                "lastLogin" TIME NOT NULL,
-                "expirationTime" TIMESTAMP NOT NULL,
-                "vkUser" BOOLEAN DEFAULT false NOT NULL,
+                "expirationTime" DATE,
+                "vkUser" BOOLEAN DEFAULT FALSE NOT NULL,
                 CONSTRAINT "userpkey" PRIMARY KEY ("id")
 );
-
 
 ALTER SEQUENCE "tradingfloor_dev"."public"."user_id_seq" OWNED BY "tradingfloor_dev"."public"."user"."id";
 
 CREATE TABLE "tradingfloor_dev"."public"."userData" (
                 "userId" INTEGER NOT NULL,
-                "photoLink" VARCHAR(100),
                 "firstName" VARCHAR(50) NOT NULL,
                 "patronymic" VARCHAR(50) NOT NULL,
                 "lastName" VARCHAR(50),
-                "contactData" VARCHAR(500) NOT NULL,
+                "contactData" TEXT NOT NULL,
+                "lastLogin" TIMESTAMP,
                 "likesAmount" INTEGER DEFAULT 0 NOT NULL,
                 "dislikesAmount" INTEGER DEFAULT 0 NOT NULL,
                 CONSTRAINT "userdatapk_fk" PRIMARY KEY ("userId")
 );
-
 
 CREATE SEQUENCE "tradingfloor_dev"."public"."tradeoffer_id_seq";
 
@@ -36,10 +33,10 @@ CREATE TABLE "tradingfloor_dev"."public"."tradeOffer" (
                 "photoLink" VARCHAR(100) NOT NULL,
                 "amount" INTEGER DEFAULT 1 NOT NULL,
                 "price" NUMERIC(2),
-                "date" TIMESTAMP NOT NULL,
-                "description" VARCHAR(1000) NOT NULL,
+                "date" DATE NOT NULL,
+                "description" TEXT NOT NULL,
                 "viewed" INTEGER DEFAULT 0 NOT NULL,
-                "status" BOOLEAN DEFAULT false NOT NULL,
+                "status" BOOLEAN DEFAULT FALSE NOT NULL,
                 "userId" INTEGER NOT NULL,
                 CONSTRAINT "tradeofferpkey" PRIMARY KEY ("id")
 );
@@ -103,3 +100,44 @@ REFERENCES "tradingfloor_dev"."public"."user" ("id")
 ON DELETE CASCADE
 ON UPDATE RESTRICT
 NOT DEFERRABLE;
+
+CREATE OR REPLACE FUNCTION public.create_user(email_in VARCHAR(50), password_in VARCHAR(300),
+        "firstName_in" VARCHAR(50), "patronymic_in" VARCHAR(50), "lastName_in" VARCHAR(50), "contactData_in" VARCHAR(500))
+  RETURNS BOOLEAN AS
+$func$
+BEGIN
+    WITH new_id  AS (
+        INSERT INTO public.user (email, password)
+            VALUES (email_in, password_in) RETURNING id
+    )
+
+    INSERT INTO public."userData" ("userId", "firstName", patronymic, "lastName", "contactData")
+    VALUES ( (SELECT id FROM new_id ), "firstName_in", "patronymic_in", "lastName_in", "contactData_in");
+    RETURN TRUE;
+  EXCEPTION
+    WHEN unique_violation THEN
+            RETURN FALSE;
+END;
+$func$  LANGUAGE plpgsql STRICT;
+
+CREATE OR REPLACE FUNCTION public.create_temp_auth(email_in VARCHAR(50), "oneTimePass_in" VARCHAR(300),
+        "expirationTime_in" DATE, "firstName_in" VARCHAR(50), "patronymic_in" VARCHAR(50), "lastName_in" VARCHAR(50), "contactData_in" VARCHAR(500))
+  RETURNS VOID AS
+$func$
+BEGIN
+    IF ( select exists(SELECT email FROM public.user WHERE email = email_in))THEN
+        UPDATE public.user SET "oneTimePass" = "oneTimePass_in", "expirationTime" = "expirationTime_in" ;
+    ELSE
+       WITH new_id  AS (
+            INSERT INTO public.user (email, "oneTimePass", "expirationTime")
+            VALUES (email_in, "oneTimePass_in", "expirationTime_in") RETURNING id
+        )
+        INSERT INTO public."userData" ("userId", "firstName", patronymic, "lastName", "contactData")
+        VALUES ( (SELECT id FROM new_id ), "firstName_in", "patronymic_in", "lastName_in", "contactData_in");
+    END IF;
+  EXCEPTION
+    WHEN unique_violation THEN
+            UPDATE public.user SET "oneTimePass" = "oneTimePass_in", "expirationTime" = "expirationTime_in" ;
+
+END;
+$func$  LANGUAGE plpgsql STRICT;
