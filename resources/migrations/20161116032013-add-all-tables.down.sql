a3d91a465e95aedc0ad7eed4be0b9cb28178e8df
hugoincer <hugoincer@gmail.com>
2016-12-08 08:53:53 +0300

    db extended with get-price func and some other

--- resources/migrations/20161116032013-add-all-tables.down.sql
+++ resources/migrations/20161116032013-add-all-tables.down.sql
@@ -2,10 +2,8 @@ DROP TABLE "tradingfloor_dev"."public"."notificationSubscription";
 
 DROP TABLE "tradingfloor_dev"."public"."tradeOffer";
 
-DROP SEQUENCE "tradingfloor_dev"."public"."tradeoffer_id_seq";
+DROP TABLE "tradingfloor_dev"."public"."currency" ;
 
 DROP TABLE "tradingfloor_dev"."public"."userData";
 
-DROP TABLE "tradingfloor_dev"."public"."user";
-
-DROP SEQUENCE "tradingfloor_dev"."public"."user_id_seq";
+DROP TABLE "tradingfloor_dev"."public"."role";
--- resources/migrations/20161116032013-add-all-tables.up.sql
+++ resources/migrations/20161116032013-add-all-tables.up.sql
@@ -1,49 +1,17 @@
+--
+--        TABLES DEFINITIONS
+--
 
-CREATE SEQUENCE "tradingfloor_dev"."public"."user_id_seq";
+CREATE SEQUENCE "tradingfloor_dev"."public"."currency_id_seq";
 
-CREATE TABLE "tradingfloor_dev"."public"."user" (
-                "id" INTEGER NOT NULL DEFAULT nextval('"tradingfloor_dev"."public"."user_id_seq"'),
-                "email" VARCHAR(50) NOT NULL,
-                "password" VARCHAR(300),
-                "oneTimePass" VARCHAR(300),
-                "expirationTime" DATE,
-                "vkUser" BOOLEAN DEFAULT FALSE NOT NULL,
-                CONSTRAINT "userpkey" PRIMARY KEY ("id")
-);
-
-ALTER SEQUENCE "tradingfloor_dev"."public"."user_id_seq" OWNED BY "tradingfloor_dev"."public"."user"."id";
-
-CREATE TABLE "tradingfloor_dev"."public"."userData" (
-                "userId" INTEGER NOT NULL,
-                "firstName" VARCHAR(50) NOT NULL,
-                "patronymic" VARCHAR(50) NOT NULL,
-                "lastName" VARCHAR(50),
-                "contactData" TEXT NOT NULL,
-                "lastLogin" TIMESTAMP,
-                "likesAmount" INTEGER DEFAULT 0 NOT NULL,
-                "dislikesAmount" INTEGER DEFAULT 0 NOT NULL,
-                CONSTRAINT "userdatapk_fk" PRIMARY KEY ("userId")
-);
-
-CREATE SEQUENCE "tradingfloor_dev"."public"."tradeoffer_id_seq";
-
-CREATE TABLE "tradingfloor_dev"."public"."tradeOffer" (
-                "id" INTEGER NOT NULL DEFAULT nextval('"tradingfloor_dev"."public"."tradeoffer_id_seq"'),
-                "name" VARCHAR(20) NOT NULL,
-                "photoLink" VARCHAR(100) NOT NULL,
-                "amount" INTEGER DEFAULT 1 NOT NULL,
-                "price" NUMERIC(2),
-                "date" DATE NOT NULL,
-                "description" TEXT NOT NULL,
-                "viewed" INTEGER DEFAULT 0 NOT NULL,
-                "status" BOOLEAN DEFAULT FALSE NOT NULL,
-                "userId" INTEGER NOT NULL,
-                CONSTRAINT "tradeofferpkey" PRIMARY KEY ("id")
+CREATE TABLE "tradingfloor_dev"."public"."currency" (
+                "id" INTEGER NOT NULL DEFAULT nextval('"tradingfloor_dev"."public"."currency_id_seq"'),
+                "name" CHAR(3) NOT NULL,
+                CONSTRAINT "currencypk" PRIMARY KEY ("id")
 );
 
 
-ALTER SEQUENCE "tradingfloor_dev"."public"."tradeoffer_id_seq" OWNED BY "tradingfloor_dev"."public"."tradeOffer"."id";
-
+ALTER SEQUENCE "tradingfloor_dev"."public"."currency_id_seq" OWNED BY "tradingfloor_dev"."public"."currency"."id";
 
 CREATE TABLE "tradingfloor_dev"."public"."notificationSubscription" (
                 "tradeOfferId" INTEGER NOT NULL,
@@ -51,9 +19,63 @@ CREATE TABLE "tradingfloor_dev"."public"."notificationSubscription" (
                 CONSTRAINT "notificationsubscriptionpk_fk" PRIMARY KEY ("tradeOfferId", "userId")
 );
 
-CREATE INDEX tradeoffer_amount_date_idx
+
+CREATE SEQUENCE "tradingfloor_dev"."public"."role_id_seq";
+
+CREATE TABLE "tradingfloor_dev"."public"."role" (
+                "id" INTEGER NOT NULL DEFAULT nextval('"tradingfloor_dev"."public"."role_id_seq"'),
+                "email" VARCHAR(50) NOT NULL,
+                "password" VARCHAR(300),
+                "oneTimePass" VARCHAR(300),
+                "expirationTime" TIMESTAMP,
+                CONSTRAINT "rolepk" PRIMARY KEY ("id")
+);
+
+
+
+
+ALTER SEQUENCE "tradingfloor_dev"."public"."role_id_seq" OWNED BY "tradingfloor_dev"."public"."role"."id";
+
+CREATE SEQUENCE "tradingfloor_dev"."public"."tradeoffer_id_seq";
+
+CREATE TABLE "tradingfloor_dev"."public"."tradeOffer" (
+                "id" INTEGER NOT NULL DEFAULT nextval('"tradingfloor_dev"."public"."tradeoffer_id_seq"'),
+                "name" VARCHAR(50) NOT NULL,
+                "price" NUMERIC(2),
+                "currencyId" INTEGER DEFAULT 1 NOT NULL,
+                "moddate" DATE NOT NULL,
+                "viewed" INTEGER DEFAULT 0 NOT NULL,
+                "status" BOOLEAN DEFAULT false NOT NULL,
+                "userId" INTEGER NOT NULL,
+                "amount" INTEGER DEFAULT 1 NOT NULL,
+                "description" VARCHAR(1000) NOT NULL,
+                "photo" BYTEA,
+                CONSTRAINT "tradeofferpk" PRIMARY KEY ("id")
+);
+
+
+ALTER SEQUENCE "tradingfloor_dev"."public"."tradeoffer_id_seq" OWNED BY "tradingfloor_dev"."public"."tradeOffer"."id";
+
+CREATE TABLE "tradingfloor_dev"."public"."userData" (
+                "userId" INTEGER NOT NULL,
+                "firstName" VARCHAR(50) NOT NULL,
+                "patronymic" VARCHAR(50) NOT NULL,
+                "lastName" VARCHAR(50),
+                "likes" INTEGER DEFAULT 0 NOT NULL,
+                "dislikes" INTEGER DEFAULT 0 NOT NULL,
+                "lastLogin" TIMESTAMP,
+                "contacts" VARCHAR(500) NOT NULL,
+                "photo" BYTEA,
+                CONSTRAINT "userdatapk_fk" PRIMARY KEY ("userId")
+);
+
+--
+--      SEARCH INDEXES
+--
+
+CREATE INDEX tradeoffer_amount_moddate_idx
  ON "tradingfloor_dev"."public"."tradeOffer"
- ( "amount", "date" );
+ ( "amount", "moddate" );
 
 CREATE INDEX tradeoffer_nad_idx
  ON "tradingfloor_dev"."public"."tradeOffer"
@@ -67,11 +89,49 @@ CREATE INDEX tradeoffer_name_idx
  ON "tradingfloor_dev"."public"."tradeOffer"
  ( "name" );
 
+--
+--      UNIQUE INDEXES
+--
+
 CREATE UNIQUE INDEX user_email_idx
- ON "tradingfloor_dev"."public"."user"
+ ON "tradingfloor_dev"."public"."role"
  ( "email" );
 
---CLUSTER tradeofferpkey ON TradeOffer;
+ CREATE UNIQUE INDEX currency_name_idx
+  ON "tradingfloor_dev"."public"."currency"
+  ( "name" );
+
+--
+--    CASCADE OPERATION CONSTRAINTS
+--
+
+ALTER TABLE "tradingfloor_dev"."public"."tradeOffer" ADD CONSTRAINT "currencytype_tradeoffer_fk"
+FOREIGN KEY ("currencyId")
+REFERENCES "tradingfloor_dev"."public"."currency" ("id")
+ON DELETE NO ACTION
+ON UPDATE NO ACTION
+NOT DEFERRABLE;
+
+ALTER TABLE "tradingfloor_dev"."public"."notificationSubscription" ADD CONSTRAINT "user_notificationsubscription_fk"
+FOREIGN KEY ("userId")
+REFERENCES "tradingfloor_dev"."public"."role" ("id")
+ON DELETE NO ACTION
+ON UPDATE NO ACTION
+NOT DEFERRABLE;
+
+ALTER TABLE "tradingfloor_dev"."public"."tradeOffer" ADD CONSTRAINT "user_tradeoffer_fk"
+FOREIGN KEY ("userId")
+REFERENCES "tradingfloor_dev"."public"."role" ("id")
+ON DELETE CASCADE
+ON UPDATE RESTRICT
+NOT DEFERRABLE;
+
+ALTER TABLE "tradingfloor_dev"."public"."userData" ADD CONSTRAINT "user_userdata_fk"
+FOREIGN KEY ("userId")
+REFERENCES "tradingfloor_dev"."public"."role" ("id")
+ON DELETE CASCADE
+ON UPDATE RESTRICT
+NOT DEFERRABLE;
 
 ALTER TABLE "tradingfloor_dev"."public"."notificationSubscription" ADD CONSTRAINT "tradeoffer_notificationsubscription_fk"
 FOREIGN KEY ("tradeOfferId")
@@ -80,39 +140,22 @@ ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
-ALTER TABLE "tradingfloor_dev"."public"."notificationSubscription" ADD CONSTRAINT "user_notificationsubscription_fk"
-FOREIGN KEY ("userId")
-REFERENCES "tradingfloor_dev"."public"."user" ("id")
-ON DELETE NO ACTION
-ON UPDATE NO ACTION
-NOT DEFERRABLE;
-
-ALTER TABLE "tradingfloor_dev"."public"."tradeOffer" ADD CONSTRAINT "user_tradeoffer_fk"
-FOREIGN KEY ("userId")
-REFERENCES "tradingfloor_dev"."public"."user" ("id")
-ON DELETE CASCADE
-ON UPDATE RESTRICT
-NOT DEFERRABLE;
-
-ALTER TABLE "tradingfloor_dev"."public"."userData" ADD CONSTRAINT "user_userdata_fk"
-FOREIGN KEY ("userId")
-REFERENCES "tradingfloor_dev"."public"."user" ("id")
-ON DELETE CASCADE
-ON UPDATE RESTRICT
-NOT DEFERRABLE;
+--
+--      STORED PROCEDURES AND FUNCTIONS
+--
 
 CREATE OR REPLACE FUNCTION public.create_user(email_in VARCHAR(50), password_in VARCHAR(300),
-        "firstName_in" VARCHAR(50), "patronymic_in" VARCHAR(50), "lastName_in" VARCHAR(50), "contactData_in" VARCHAR(500))
+        "firstName_in" VARCHAR(50), "patronymic_in" VARCHAR(50), "lastName_in" VARCHAR(50), "contacts_in" VARCHAR(500))
   RETURNS BOOLEAN AS
 $func$
 BEGIN
     WITH new_id  AS (
-        INSERT INTO public.user (email, password)
+        INSERT INTO public.role (email, password)
             VALUES (email_in, password_in) RETURNING id
     )
 
-    INSERT INTO public."userData" ("userId", "firstName", patronymic, "lastName", "contactData")
-    VALUES ( (SELECT id FROM new_id ), "firstName_in", "patronymic_in", "lastName_in", "contactData_in");
+    INSERT INTO public."userData" ("userId", "firstName", patronymic, "lastName", contacts)
+    VALUES ( (SELECT id FROM new_id ), "firstName_in", "patronymic_in", "lastName_in", "contacts_in");
     RETURN TRUE;
   EXCEPTION
     WHEN unique_violation THEN
@@ -121,23 +164,31 @@ END;
 $func$  LANGUAGE plpgsql STRICT;
 
 CREATE OR REPLACE FUNCTION public.create_temp_auth(email_in VARCHAR(50), "oneTimePass_in" VARCHAR(300),
-        "expirationTime_in" DATE, "firstName_in" VARCHAR(50), "patronymic_in" VARCHAR(50), "lastName_in" VARCHAR(50), "contactData_in" VARCHAR(500))
+        "expirationTime_in" DATE, "firstName_in" VARCHAR(50), "patronymic_in" VARCHAR(50), "lastName_in" VARCHAR(50), "contacts_in" VARCHAR(500))
   RETURNS VOID AS
 $func$
 BEGIN
-    IF ( select exists(SELECT email FROM public.user WHERE email = email_in))THEN
-        UPDATE public.user SET "oneTimePass" = "oneTimePass_in", "expirationTime" = "expirationTime_in" ;
+    IF ( select exists(SELECT email FROM public.role WHERE email = email_in))THEN
+        UPDATE public.role SET "oneTimePass" = "oneTimePass_in", "expirationTime" = "expirationTime_in" ;
     ELSE
        WITH new_id  AS (
-            INSERT INTO public.user (email, "oneTimePass", "expirationTime")
+            INSERT INTO public.role (email, "oneTimePass", "expirationTime")
             VALUES (email_in, "oneTimePass_in", "expirationTime_in") RETURNING id
         )
-        INSERT INTO public."userData" ("userId", "firstName", patronymic, "lastName", "contactData")
-        VALUES ( (SELECT id FROM new_id ), "firstName_in", "patronymic_in", "lastName_in", "contactData_in");
+        INSERT INTO public."userData" ("userId", "firstName", patronymic, "lastName", contacts)
+        VALUES ( (SELECT id FROM new_id ), "firstName_in", "patronymic_in", "lastName_in", "contacts_in");
     END IF;
   EXCEPTION
     WHEN unique_violation THEN
-            UPDATE public.user SET "oneTimePass" = "oneTimePass_in", "expirationTime" = "expirationTime_in" ;
+            UPDATE public.role SET "oneTimePass" = "oneTimePass_in", "expirationTime" = "expirationTime_in" ;
 
+END;
+$func$  LANGUAGE plpgsql STRICT;
+
+CREATE OR REPLACE FUNCTION public.get_price_id(name_in CHAR(3))
+  RETURNS INTEGER AS
+$func$
+BEGIN
+RETURN (SELECT id FROM public.currency WHERE name = name_in);
 END;
 $func$  LANGUAGE plpgsql STRICT;
